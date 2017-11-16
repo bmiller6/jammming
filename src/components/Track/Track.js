b10795d5467e020f74bdb435faa4d09a146f9ab8
bmiller6 <brycemiller6@gmail.com>
2017-11-15 19:18:48 -0800

    Update Track.js

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,53 +1,26 @@
 import React from 'react';
-import './Track.css';
+import './TrackList.css';
+import Track from '../Track/Track';
 
-class Track extends React.Component {
-  constructor(props) {
-    super(props);
-    this.addTrack = this.addTrack.bind(this);
-    this.removeTrack = this.removeTrack.bind(this);
-  }
-
-  addTrack(event) {
-    return this.props.onAdd(this.props.track);
-  }
-
-  removeTrack(event) {
-    return this.props.onRemove(this.props.track);
-  }
-
-  renderTrackAction() {
-    if (!this.props.isRemoval) {
-      return (
-        <a
-          className="Track-action"
-          onClick={this.addTrack}>
-          +
-        </a>
-      );
-    }
-    else {
-      return (
-        <a
-          className="Track-action"
-          onClick={this.removeTrack}>
-          -
-        </a>
-      );
-    }
-  }
-
+class TrackList extends React.Component {
   render() {
     return (
-      <div className="Track">
-        <div className="Track-information">
-          <h3>{this.props.track.name}</h3>
-          <p>{this.props.track.artist} | {this.props.track.album}</p>
-        </div>
-        {this.renderTrackAction()}
+      <div className="TrackList">
+          {this.props.tracks && this.props.tracks.map(track => {
+            return (
+              <Track
+                key={track.id}
+                track={track}
+                onAdd={this.props.onAdd}
+                onRemove={this.props.onRemove}
+                isRemoval={this.props.isRemoval}
+              />
+            )
+          }
+        )}
       </div>
     );
   }
 }
 
-export default Track;
+export default TrackList;
